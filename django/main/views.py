d126d2b21328ae604ce76d90b270c33daa96414b
alexandre <alexandre.tresallet@gmail.com>
2024-12-06 15:07:55 +0100

    create base for django

---
+++ django/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import User
+
+# Create your views here.
+def index(req):
+    users = User.objects.all()
+    context = {"users":users}
+    return render(req, "index.html", context)
