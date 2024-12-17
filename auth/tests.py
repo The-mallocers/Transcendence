ae1feb1fdec2dd30a6235df3ab1bad96c820b3e6
Alexandre <alexandre.tresallet@gmail.com>
2024-12-17 13:23:43 +0100

    Add CI workflow, update .gitignore, and enhance tests
    Introduce GitHub Actions CI workflow for Django with Python 3.7-3.9. Adjust .gitignore to rename test database file and expand authentication tests to include basic login functionality. These changes streamline testing and improve project readiness.

--- auth/tests.py
+++ auth/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+class AuthTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test(self):
+        url = '/auth/login'
+        data = {
+            "email": "alexandre.tresallet@gmail.com",
+            "password": "1234"
+        }
+        response = self.client.get('/auth/login')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
