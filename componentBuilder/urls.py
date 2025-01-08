b3b1be7a8afdd8c26799b3adaef8280044d22b59
Alexandre <alexandre.tresallet@gmail.com>
2025-01-08 19:33:17 +0100

    Update .gitignore and adjust URL patterns in componentBuilder
    Renamed the SQLite database entry in .gitignore to reflect the new filename. Updated URL patterns by setting the editMode view as the default route, improving intuitive navigation.

--- componentBuilder/urls.py
+++ componentBuilder/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('edit', views.editMode),
+    path('', views.editMode),
     path('save-component', views.saveComponent),
     path('get-component/<int:component_id>', views.getComponent),
 ]
