51fe26d7f9accec284da56640eb9248edee940ef
Alexandre <alexandre.tresallet@gmail.com>
2025-01-17 18:02:55 +0100

    Remove test from auth/tests.py

--- auth/tests.py
+++ auth/tests.py
@@ -1,18 +1,1 @@
-from django.test import TestCase, Clients
 
-
-# Create your tests here.
-class AuthTest(TestCase):
-    def setUp(self):
-        self.client = Clients()
-
-    def test(self):
-        url = '/auth/login'
-        data = {
-            "email": "alexandre.tresallet@gmail.com",
-            "password": "1234"
-        }
-        response = self.client.get('/auth/login')
-        self.assertEqual(response.status_code, 200)
-        # response = self.client.post(url, data)
-        # self.assertEqual(response.status_code, 400)
