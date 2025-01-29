057e42bdff2d2147fd9f03c5417e3497bd9947b5
Theo Freydiere <moitheo2@hotmail.fr>
2025-01-29 18:23:51 +0100

    I got the main system I think, id like to implement this with main in the future

--- static/js/spa/spa.js
+++ static/js/spa/spa.js
@@ -2,16 +2,27 @@ async function handleRoute() {
     const path = window.location.pathname;
     const mainContent = document.querySelector('#app');
     
+    let response;
+    let content;
+
     switch(path) {
         case '/':
-            const response = await fetch('/api/auth/login', {
+            response = await fetch('/api/auth/login', {
                 headers: {
                     'X-CSRFToken': document.querySelector('meta[name="csrf-token"]').content
                 }
             });
-            const content = await response.json();
+            content = await response.json();
             mainContent.innerHTML = content.html;
             break;
+        default:
+            response = `<div style="text-align: center; padding: 50px;">
+                    <h1>404 - Page Not Found</h1>
+                    <p>Sorry, the page you are looking for does not exist.</p>
+                </div>`;
+            mainContent.innerHTML = response
+            break;
+
     }
 }
 
