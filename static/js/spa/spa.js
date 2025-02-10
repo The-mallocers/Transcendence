185cdafed5e8c6e5ba8e8a11b6d30766867c43fa
Theo Freydiere <moitheo2@hotmail.fr>
2025-02-10 18:32:59 +0100

    broke the register link, woopsie, fixed it now

--- static/js/spa/spa.js
+++ static/js/spa/spa.js
@@ -133,7 +133,7 @@ const routes = [
     {
         path: '/auth/register',
         template: async () => {
-            return await fetchRoute('/pages/auth/register/');
+            return await fetchRoute('/pages/auth/register');
         },
     },
 ];
