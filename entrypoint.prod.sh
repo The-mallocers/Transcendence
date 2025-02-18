41336ecc5ce14fb798dc49df5102fd61d2dfccd0
bastpoy <bastien.poyet@outlook.fr>
2025-02-18 16:50:06 +0100

    docker ok listening on port 8080 need to do https

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# echo "Waiting for postgres..."
-# while ! nc -z db 5432; do
-#   echo "waiting db"
-#   sleep 0.1
-# done
-
 echo "PostgreSQL started"
 
 python manage.py collectstatic --noinput
