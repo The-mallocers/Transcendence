802bacb7b09c3202e6ee2098e4dfb4c156b5dc08
Theo Freydiere <moitheo2@hotmail.fr>
2025-03-07 15:13:21 +0100

    added very precious line to set up env var

--- manage.py
+++ manage.py
@@ -7,13 +7,14 @@ import django
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
+    #we will ALWAYS want to set up the above
     if os.environ.get('DAPHNE', default=False) == 'TRUE':
         sys.argv = ['daphne', 'config.asgi:application']
         from daphne.cli import CommandLineInterface
         django.setup()
         CommandLineInterface.entrypoint()
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
         from django.core.management import execute_from_command_line
         execute_from_command_line(sys.argv)
 
