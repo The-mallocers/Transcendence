75abc349ca91ccc529ec31cc7963a554f64ce1dd
Theo Freydiere <moitheo2@hotmail.fr>
2025-03-12 14:19:42 +0100

    added back the manage.py of the future

--- manage.py
+++ manage.py
@@ -8,14 +8,14 @@ import django
 def main():
     """Run administrative tasks."""
     #we will ALWAYS want to set up the above
-    if os.environ.get('DAPHNE', default=False) == 'TRUE':
-        os.environ.setdefault('DAPHNE','TRUE') #Adding this so i dont have to do it everytime
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
+    
+    if "runserver" in sys.argv:
         sys.argv = ['daphne', 'config.asgi:application']
         from daphne.cli import CommandLineInterface
         django.setup()
         CommandLineInterface.entrypoint()
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
         from django.core.management import execute_from_command_line
         execute_from_command_line(sys.argv)
 
