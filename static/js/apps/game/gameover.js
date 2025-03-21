956a75b54b01f8185b7ef44011ebee688adecf79
Theo Freydiere <moitheo2@hotmail.fr>
2025-03-21 17:09:28 +0100

    on a fix le probleme de gameover et c'est encore une fois une petite blague avec js

--- static/js/apps/game/gameover.js
+++ static/js/apps/game/gameover.js
@@ -1,14 +1,14 @@
-let endStateThingy = ()=>{
-    let msg = document.querySelector(".endState");
-    let words = msg.innerHTML.trim().split(" ");
-    let newContent = words.map(word => 
-        word.split("").map((letter, letterIndex, arr) => 
-            `<span class="${letterIndex === arr.length - 1 ? "wordEnd" : ""}">${letter}</span>`
-        ).join("")
-    ).join(" ");    
-
-    msg.innerHTML = newContent;
-    console.log(newContent)
+function endStateThingy(event) {
+        const msg = document.querySelector(".endState");
+        const words = msg.innerHTML.trim().split(" ");
+        const newContent = words.map(word => 
+            word.split("").map((letter, letterIndex, arr) => 
+                `<span class="${letterIndex === arr.length - 1 ? "wordEnd" : ""}">${letter}</span>`
+            ).join("")
+        ).join(" ");
+    
+        msg.innerHTML = newContent;
+        console.log(newContent)
 }
 
-endStateThingy()
+endStateThingy();
