6a95fba4f660c028c0b8d472c36fea2fe41e552c
Theo Freydiere <moitheo2@hotmail.fr>
2025-03-25 18:58:26 +0100

    I am probably the biggest moron but the ball isnt rendering, hopefully next time i look at this i can see that the condition are obviously wrong and will fix it in 15 seconds

--- docker/staticdocker/js/spa/spa.js
+++ docker/staticdocker/js/spa/spa.js
@@ -1,4 +1,5 @@
 import { WebSocketManager } from "../websockets/websockets.js"
+import { isGameOver } from "../apps/game/VarGame.js"
 
 
 class Router {
@@ -69,7 +70,9 @@ class Router {
         else {
             WebSocketManager.closeAllSockets(); //for now we close all
         }
-
+        if (window.location.pathname == path) {return ;}
+        
+        isGameOver.gameIsOver = true;
         //In the future, we will have to do some better logics with the path to decide if we want to close
         //a websocket or not.
         
