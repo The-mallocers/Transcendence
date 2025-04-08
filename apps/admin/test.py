f44d1ba49f1ec44be56133b7badeb9ce112f8662
Alexandre <alexandre.tresallet@gmail.com>
2025-04-08 16:09:01 +0200

    Add all the test file for each apps

--- apps/admin/test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from apps.client.models import Clients
-
-
-class AdminTestCase(TestCase):
-    def test_animals_can_speak(self):
-        admin = Clients.objects.get(profile__username='admin')
-        self.assertEqual(admin.profile.email, 'admin@transcendence.fr')
