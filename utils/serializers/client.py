beaca12ba8dbf9468afd96fd51710690b76a6434
Alexandre <alexandre.tresallet@gmail.com>
2025-04-14 11:21:17 +0200

    Fix issue with admin not have is_admin flags to true

--- utils/serializers/client.py
+++ utils/serializers/client.py
@@ -2,9 +2,9 @@ from django.db import transaction
 from rest_framework import serializers
 
 from apps.admin.models import Rights
-from apps.notifications.models import Friend
 from apps.auth.models import Password, TwoFA
 from apps.client.models import Clients, Stats
+from apps.notifications.models import Friend
 from apps.profile.models import Profile
 from utils.serializers.auth import PasswordSerializer
 from utils.serializers.profile import ProfileSerializer
@@ -21,7 +21,8 @@ class ClientSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         profile_data = validated_data.pop('profile')
         password_data = validated_data.pop('password')
-        
+        is_admin = self.context.get('is_admin', False)
+
         # Removing passwordcheck since its not longer useful
         if 'passwordcheck' in password_data:
             password_data.pop('passwordcheck')
@@ -31,14 +32,10 @@ class ClientSerializer(serializers.ModelSerializer):
                 profile = Profile.objects.create(**profile_data)
                 passwrod = Password.objects.create(**password_data)
                 two_fa = TwoFA.objects.create()
-                right = Rights.objects.create(is_admin=False)
+                right = Rights.objects.create(is_admin=is_admin)
                 friend = Friend.objects.create()
                 stats = Stats.objects.create()
                 client = Clients.objects.create(profile=profile, password=passwrod, twoFa=two_fa, rights=right, friend=friend, stats=stats)
-
-                if validated_data.get('is_admin', False):
-                    client.rights.is_admin = True
-                    client.rights.save()
 
         except Exception as e:
             raise serializers.ValidationError(f"Error creating client: {str(e)}")
