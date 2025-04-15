ed27b85f081e3e3a30616b9da1b7313d79733863
Alexandre <alexandre.tresallet@gmail.com>
2025-04-15 18:17:18 +0200

    Create pending friend api endpoint

--- apps/notifications/api/views.py
+++ apps/notifications/api/views.py
@@ -1,40 +1,27 @@
 from django.http import HttpRequest
 from rest_framework import status
-from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 from rest_framework.views import APIView
 
 from apps.client.models import Clients
-from apps.notifications.models import Friend
 
 
-class FriendsSerializer(ModelSerializer):
-    friends = PrimaryKeyRelatedField(many=True, read_only=True)
-
+class FriendSerializer(ModelSerializer):
     class Meta:
-        model = Friend
-        fields = 'friends'
-
-
-class PendingFriendsSerializer(ModelSerializer):
-    pending_friends = PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Friend
-        fields = 'pending_friends'
+        model = Clients
+        fields = ['id']
 
 
 class GetFriendsApiView(APIView):
     def get(self, request: HttpRequest, *args, **kwargs):
         client = Clients.get_client_by_request(request)
-        serializer = FriendsSerializer(client.friend, many=True)
-        print(serializer.data)
+        serializer = FriendSerializer(client.friend.friends.all(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class GetPendingFriendsApiView(APIView):
     def get(self, request: HttpRequest, *args, **kwargs):
-        return Response({
-            "client_id"
-        }, status=status.HTTP_200_OK)
+        client = Clients.get_client_by_request(request)
+        serializer = FriendSerializer(client.friend.pending_friends.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
