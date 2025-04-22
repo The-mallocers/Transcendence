f4c1d4ff256f9d12092966e253bacfb32165403d
Alexandre <alexandre.tresallet@gmail.com>
2025-04-22 11:25:24 +0200

    Refactor pending duels retrieval to use Redis scanning.
    Replaced database query with Redis scan to retrieve pending duels efficiently. Leveraged RedisConnectionPool and RTables for accessing duel-related data. Improved performance by filtering duels directly in Redis instead of querying all pending duels.

--- apps/notifications/api/views.py
+++ apps/notifications/api/views.py
@@ -6,6 +6,8 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.views import APIView
 
 from apps.client.models import Clients
+from utils.enums import RTables
+from utils.redis import RedisConnectionPool
 
 
 class FriendSerializer(ModelSerializer):
@@ -33,5 +35,11 @@ class GetPendingFriendsApiView(APIView):
 class GetPendingDuelsApiView(APIView):
     def get(self, request: HttpRequest, *args, **kwargs):
         client = Clients.get_client_by_request(request)
-        serializer = FriendSerializer(client.friend.pending_duel.all(), many=True)
+        redis = RedisConnectionPool.get_sync_connection(self.__class__.__name__)
+        pending_duels = []
+        for duel_hash in redis.scan_iter(match=RTables.HASH_DUEL_QUEUE('*')):
+            if redis.hexists(duel_hash, str(client.id)):
+                pending_duels.append(duel_hash)
+        targets = [redis.hgetall(duel_hash) for duel_hash in pending_duels]
+        serializer = FriendSerializer(Clients.objects.filter(id__in=[x['target'] for x in targets]), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
