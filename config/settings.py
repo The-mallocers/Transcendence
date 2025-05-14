a3241f11ee031495a785838329c74c380f658734
bastpoy <bastien.poyet@outlook.fr>
2025-05-14 10:18:44 +0200

    modify settings.py

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ REST_FRAMEWORK = {
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SECURITY SETTINGS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ #
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'z2r6p5']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'z1r5p2']
 CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'https://127.0.0.1:8000']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
