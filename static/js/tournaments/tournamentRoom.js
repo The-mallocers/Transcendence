0b04a6182d12337aa2c8e0122f94281ee11d4557
llitot <lenylitot@gmail.com>
2025-05-16 18:42:34 +0200

    invite trounament

--- static/js/tournaments/tournamentRoom.js
+++ static/js/tournaments/tournamentRoom.js
@@ -6,6 +6,48 @@ const tournamentSocket = WebSocketManager.tournamentSocket;
 const delete_btn = document.querySelector("#delete-btn");
 const leave_btn = document.querySelector("#leave-btn");
 
+import {apiFriends} from "../profile/profile.js";
+import {WebSocketManager} from "../../websockets/websockets.js";
+
+const pathname = window.location.pathname;
+console.log(pathname);
+const notifSocket = WebSocketManager.notifSocket;
+console.log(notifSocket);
+
+if (pathname == "/pong/tournament/") {
+    console.log("tournament room");
+    const friends = await apiFriends("/api/friends/get_friends/");
+    const duelFriends = document.querySelector(".friends-to-duel");
+    console.log(friends);
+    if (!friends.length) {
+        const parser = new DOMParser();
+        const html =
+            `<li class="list-group-item d-flex justify-content-between align-items-center">
+            <div>No friend to duel</div>
+        </li>`
+        const doc = parser.parseFromString(html, "text/html");
+        const friendElement = doc.body.firstChild;
+        duelFriends.appendChild(friendElement);
+    }
+    friends.forEach(friend => {
+        const parser = new DOMParser();
+        const html =
+            `<li class="list-group-item d-flex justify-content-between align-items-center">
+            <div>${friend.username}</div>
+            <div class="d-flex align-items-center">
+                <button type="button" class="type-intra-green duel_friend me-4">Duel</button>
+            </div>
+        </li>`
+        const doc = parser.parseFromString(html, "text/html");
+        const friendElement = doc.body.firstChild;
+        const duelButton = friendElement.querySelector('.duel_friend');
+        duelButton.addEventListener('click', function () {
+            hide_modal(friend.id)
+        });
+        duelFriends.appendChild(friendElement);
+    });
+}
+
 function leaveTournament() {
     WebSocketManager.closeTournamentSocket();
     navigateTo("/pong/gamemodes/");
@@ -24,6 +66,22 @@ const get_tournament_info = {
     "data": {
         "action": "tournament_info"
     }    
+}
+
+window.invite_friends = function(){
+    const friendSelectionModal = new bootstrap.Modal(document.querySelector('#friendSelectionModal'));
+    friendSelectionModal.show();
+}
+
+window.hide_modal = async function (usernameId) {
+    try {
+        const modal = bootstrap.Modal.getInstance(document.getElementById('friendSelectionModal'));
+        modal.hide();
+        // const message = create_message_duel("create_duel", usernameId);
+        // notifSocket.send(JSON.stringify(message));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 sendWhenReady(tournamentSocket, JSON.stringify(get_tournament_info));
