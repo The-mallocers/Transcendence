df1080855c4aba8f60420170590ea99f5e20e728
EZ3C <mboyer980@gmail.com>
2025-05-19 00:10:14 +0200

    tried to change password implementation because it is made in a very unconventional way but there was too many things to change, so i just made a patch fix, people can't have $ in their passwords

--- utils/serializers/auth.py
+++ utils/serializers/auth.py
@@ -27,6 +27,9 @@ class PasswordSerializer(serializers.ModelSerializer):
 
     def validate_password(self, value):
         instance = self.instance
+
+        if '$' in value:
+            raise serializers.ValidationError("Password cannot contain the '$' character.")
         if instance and not instance.check_pwd(value):
             print("Hello sir")
             raise serializers.ValidationError("New password must be different from the old password.")
