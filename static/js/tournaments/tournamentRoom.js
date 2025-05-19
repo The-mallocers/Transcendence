1340b681ab5f9836716b0343ef96951b1b4f3f66
llitot <lenylitot@gmail.com>
2025-05-19 12:20:56 +0200

    implementation du front pour les invitations au tournoi, il faut faire le back maintenant

--- static/js/tournaments/tournamentRoom.js
+++ static/js/tournaments/tournamentRoom.js
@@ -1,52 +1,12 @@
 import { WebSocketManager } from "../websockets/websockets.js"
 import { navigateTo } from "../spa/spa.js"
 import { sendWhenReady } from "../utils/utils.js";
+import { apiFriends } from "../apps/profile/profile.js";
 
 const tournamentSocket = WebSocketManager.tournamentSocket;
 const delete_btn = document.querySelector("#delete-btn");
 const leave_btn = document.querySelector("#leave-btn");
 
-import {apiFriends} from "../profile/profile.js";
-import {WebSocketManager} from "../../websockets/websockets.js";
-
-const pathname = window.location.pathname;
-console.log(pathname);
-const notifSocket = WebSocketManager.notifSocket;
-console.log(notifSocket);
-
-if (pathname == "/pong/tournament/") {
-    console.log("tournament room");
-    const friends = await apiFriends("/api/friends/get_friends/");
-    const duelFriends = document.querySelector(".friends-to-duel");
-    console.log(friends);
-    if (!friends.length) {
-        const parser = new DOMParser();
-        const html =
-            `<li class="list-group-item d-flex justify-content-between align-items-center">
-            <div>No friend to duel</div>
-        </li>`
-        const doc = parser.parseFromString(html, "text/html");
-        const friendElement = doc.body.firstChild;
-        duelFriends.appendChild(friendElement);
-    }
-    friends.forEach(friend => {
-        const parser = new DOMParser();
-        const html =
-            `<li class="list-group-item d-flex justify-content-between align-items-center">
-            <div>${friend.username}</div>
-            <div class="d-flex align-items-center">
-                <button type="button" class="type-intra-green duel_friend me-4">Duel</button>
-            </div>
-        </li>`
-        const doc = parser.parseFromString(html, "text/html");
-        const friendElement = doc.body.firstChild;
-        const duelButton = friendElement.querySelector('.duel_friend');
-        duelButton.addEventListener('click', function () {
-            hide_modal(friend.id)
-        });
-        duelFriends.appendChild(friendElement);
-    });
-}
 
 function leaveTournament() {
     WebSocketManager.closeTournamentSocket();
@@ -68,22 +28,65 @@ const get_tournament_info = {
     }    
 }
 
-window.invite_friends = function(){
+sendWhenReady(tournamentSocket, JSON.stringify(get_tournament_info));
+
+window.invite_friends = async function() {
     const friendSelectionModal = new bootstrap.Modal(document.querySelector('#friendSelectionModal'));
+    await populateFriendsModal();
     friendSelectionModal.show();
 }
 
-window.hide_modal = async function (usernameId) {
-    try {
-        const modal = bootstrap.Modal.getInstance(document.getElementById('friendSelectionModal'));
-        modal.hide();
-        // const message = create_message_duel("create_duel", usernameId);
-        // notifSocket.send(JSON.stringify(message));
-    } catch (error) {
-        console.log(error);
+async function populateFriendsModal() {
+    const friends = await apiFriends("/api/friends/get_friends/");
+    const inviteFriends = document.querySelector(".friends-to-invite");
+    
+    inviteFriends.innerHTML = '';
+    
+    if (!friends.length) {
+        const parser = new DOMParser();
+        const html =
+            `<li class="list-group-item d-flex justify-content-between align-items-center">
+            <div>No friends to invite</div>
+        </li>`
+        const doc = parser.parseFromString(html, "text/html");
+        const friendElement = doc.body.firstChild;
+        inviteFriends.appendChild(friendElement);
+    } else {
+        friends.forEach(friend => {
+            const parser = new DOMParser();
+            const html =
+                `<li class="list-group-item d-flex justify-content-between align-items-center">
+                <div>${friend.username}</div>
+                <div class="d-flex align-items-center">
+                    <button type="button" class="type-intra-green invite_friend me-4">Invite</button>
+                </div>
+            </li>`
+            const doc = parser.parseFromString(html, "text/html");
+            const friendElement = doc.body.firstChild;
+            const inviteButton = friendElement.querySelector('.invite_friend');
+            inviteButton.addEventListener('click', function () {
+                sendInvitation(friend.id);
+            });
+            inviteFriends.appendChild(friendElement);
+        });
     }
 }
 
-sendWhenReady(tournamentSocket, JSON.stringify(get_tournament_info));
+function sendInvitation(friendId) {
+    const modal = bootstrap.Modal.getInstance(document.getElementById('friendSelectionModal'));
+    modal.hide();
+    
+    console.log(`Invitation sent to friend with ID: ${friendId}`);
+    const inviteMessage = {
+        "event": "tournament",
+        "data": {
+            "action": "invite_friend",
+            "args": {
+                "friend_id": friendId
+            }
+        }
+    };
+    console.log("Invitation message:", inviteMessage);
+}
 
 
