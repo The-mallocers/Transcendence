d42dd2c337f50858cc4d8346cf09ecba6da1f87d
Theo Freydiere <moitheo2@hotmail.fr>
2025-05-22 17:01:24 +0200

    user now gets redirected if he is logged in and tries to go to login or register page

--- apps/auth/views/login.py
+++ apps/auth/views/login.py
@@ -7,14 +7,20 @@ from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from django.template.loader import render_to_string
 
+from apps.auth.api.views import formulate_json_response
 from apps.client.models import Clients
 
 grafana_id = 0
 
 
 def get(req):
+    try:
+        client = Clients.get_client_by_request(req)
+        if client:
+            return formulate_json_response(True, 302, "You are logged in !", "/")
+    except Exception as e:
+        pass
     csrf_token = get_token(req)
-    users = Clients.objects.all()
     html_content = render_to_string("apps/auth/login.html", {
         "csrf_token": csrf_token,
     })
