644e54eb16b75d4776b6b63fe8a9a5465268006e
Mathieu Boyer <mboyer980@gmail.com>
2025-05-23 08:33:01 +0200

    i think it works now let's test together to be sure tho

--- apps/error/urls.py
+++ apps/error/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
 from . import views
+from django.conf.urls import handler404
+
+handler404 = 'views.error'
 
 urlpatterns = [
     path('<int:error_code>/', views.error, name='error'),
