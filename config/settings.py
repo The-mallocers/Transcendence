e9982e81fa625f3e355ecb7cc657c230d94d1bd1
Theo Freydiere <moitheo2@hotmail.fr>
2025-05-23 17:20:11 +0200

    added something so wrong requests dont look so legit

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SECURITY SETTINGS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ #
