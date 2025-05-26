d3fdf64d61206c4c12c797978336a2587bedf6df
Theo Freydiere <moitheo2@hotmail.fr>
2025-05-26 16:15:15 +0200

    petit hotfix du nginx pour foutre la limite max body size au bonne endroit pour upload des photos HD

---
+++ static/js/apps/game/localover.js
@@ -0,0 +1,9 @@
+const left_score = document.getElementById('myScore');
+const right_score = document.getElementById('enemyScore');
+
+
+if (left_score) {
+    left_score.innerText = window.Gamestate
+}
+
+
