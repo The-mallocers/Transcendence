40de76c121cd341edac3c0636a0e0ecb0d95ef05
bastpoy <bastien.poyet@outlook.fr>
2025-05-29 16:42:25 +0200

    ca fix le front en legende dans le gova direction le sud

--- static/js/utils/utils.js
+++ static/js/utils/utils.js
@@ -36,10 +36,10 @@ export function create_front_chat_room(room, username, usernameId, status, profi
         const parser = new DOMParser();
         const htmlChat = 
         `<div class="roomroom container d-flex flex-wrap align-items-center justify-content-between">
-            <button class="chat-${username} chat-button btn d-flex align-items-center gap-3">
+            <div class="chat-${username} chat-button btn d-flex align-items-center gap-3">
                 <img src="${imgSrc}" alt="${username}'s profile picture">
                 <div>${username}</div>
-            </button>
+            </div>
             
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle no-caret rounded-circle" type="button" id="dropdownMenu-${room}" data-bs-toggle="dropdown" aria-expanded="false">
@@ -54,27 +54,31 @@ export function create_front_chat_room(room, username, usernameId, status, profi
         </div>`
         const doc = parser.parseFromString(htmlChat, "text/html");
         const chatElement = doc.body.firstChild;
-        const chatButton = chatElement.querySelector(`.chat-${username}`)
+        // const chatButton = chatElement.querySelector(`.chat-${username}`)
+        // const roomroom = chatElement.querySelector(".roomroom")
         const chatProfile = chatElement.querySelector(`.chat-profile`);
         const chatBlock = chatElement.querySelector(`.chat-block`);
         const chatDuel = chatElement.querySelector(`.chat-duel`);
-
-        chatButton?.addEventListener('click', function() {
-            const roomroomDiv = this.closest('.roomroom');
-    
-            roomroomDiv.classList.add('active-room');
+        // console.log(roomroom);
+        console.log(chatElement)
+        chatElement?.addEventListener('click', function() {
+            // const roomroomDiv = this.closest('.roomroom');
+            console.log("je clique");
+            this.classList.add('active-room');
             
             document.querySelectorAll('.roomroom.active-room').forEach(div => {
-                if (div !== roomroomDiv) {
+                if (div !== this) {
                     div.classList.remove('active-room');
                 }
             });
             clickRoom(room)
         })
-        chatProfile?.addEventListener('click', function(){
+        chatProfile?.addEventListener('click', function(event){
+            event.stopPropagation();
             handleChatProfile(username);
         })
-        chatBlock?.addEventListener('click', function(){
+        chatBlock?.addEventListener('click', function(event){
+            event.stopPropagation();
             if(this.innerHTML == "Block"){
                 this.innerHTML = "Unblock"
                 handleChatBlock(username);
@@ -85,9 +89,13 @@ export function create_front_chat_room(room, username, usernameId, status, profi
             }
             
         })
-        chatDuel?.addEventListener('click', function(){
+        chatDuel?.addEventListener('click', function(event){
+            event.stopPropagation();
             handleChatDuel(usernameId);
         })
+        chatElement.querySelector('.dropdown').addEventListener('click', function(event) {
+            event.stopPropagation();
+        });
         newChat.appendChild(chatElement);
     }
 }
