c8b3416880de270e09e3ad5fc190ade13c4f03c1
Theo Freydiere <moitheo2@hotmail.fr>
2025-06-02 12:52:22 +0200

    some pretty fix for tournament query room

--- static/js/tournaments/treeQuery.js
+++ static/js/tournaments/treeQuery.js
@@ -3,11 +3,26 @@ import { populateTree } from "./populateHelpers.js";
 if (window.treeInfos){
     populateTree(window.treeInfos)
     setTimeout(()=>{
-        const leaveBtn = document.querySelector("#leave-btn-tree")
-        const readyBtn = document.querySelector("#ready-btn")
-        if (leaveBtn)
-            leaveBtn.innerText = "Back to gamemodes"
+        const leaveBtn = document.querySelector("#leave-btn-tree");
+        const readyBtn = document.querySelector("#ready-btn");
+        const titleTree = document.querySelector("h3");
+        if (titleTree) {
+            titleTree.innerText = "Tournament Result";
+            // titleTree.parentElement.classList.add('text-center');
+            titleTree.parentElement.classList.remove('justify-content-between');
+            titleTree.parentElement.classList.add('justify-content-center');
+            titleTree.style.fontWeight = 'bold';
+            titleTree.style.fontSize = '2.5rem';
+        }
+        if (leaveBtn) {
+            leaveBtn.innerText = "Go back"
+            leaveBtn.removeAttribute('data-route');
+            leaveBtn.onclick = function() {
+                history.back();
+            }
+        }
         if (readyBtn)
             readyBtn.remove()
     }, 0)
+
 }
